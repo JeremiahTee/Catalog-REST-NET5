5063304a22bce41cfb0a44193aa465e045109389
Jeremiah Tiongson <jeremiahteee@gmail.com>
2023-06-19 16:18:43 -0400

    Item model, controller, hardcoded repository

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using Catalog.Models;
+using Catalog.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers {
+    [ApiController]
+    [Route("items")]
+    public class ItemsController: ControllerBase {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController(){
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if(item is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(item);
+        }
+    }
+}
